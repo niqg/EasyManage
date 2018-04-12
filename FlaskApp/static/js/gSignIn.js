d6e0299c52bd340e0f3d17f47d9aa7cbeb051217
00Bren00 <ringelb8@students.rowan.edu>
2018-04-11 21:42:27 -0400

    Update gSignIn.js
    more user functionality. does not let someone login unless they are in the database. still need to create add new user functionality.

--- FlaskApp/static/js/gSignIn.js
+++ FlaskApp/static/js/gSignIn.js
@@ -1,11 +1,33 @@
 function onSignIn(googleUser)
 {
-    var googleToken = "tempID";
-    //Hijacked, LOL
-    $.ajax({url:"/login", type: "POST", data: {google_client_id: googleToken}, success: function(data) {
-        console.log(data.key);
-        window.location = "/home";
+    var SUCCESS_KEY = 0;
+    var ERROR_KEY = 1;
+    var WARNING_KEY = 2;
+
+    var profile = googleUser.getBasicProfile();
+    var theUserEmail = profile.getEmail();
+    console.log(theUserEmail);
+
+
+    $.ajax({url:"/users/get", type: "GET", data: {userEmail: theUserEmail}, success: function(data) {
+        //console.log(data.key);
+        if(data.key == SUCCESS_KEY) //user exists
+        {
+            
+            var googleToken = "tempID";
+            //Hijacked, LOL
+            //$.ajax({url:"/login", type: "POST", data: {google_client_id: googleToken}, success: function(data) {
+            window.location = "/home";
+        }
+        else                        //prompt user to make an account
+        {
+
+            var modal = document.getElementById("loginModule");
+            modal.innerhtml = "";
+        
+        }
     }});
+    
 
     //var profile=googleUser.getBasicProfile();
     //$(".g-signin2").css("display", "none");
@@ -13,4 +35,7 @@ function onSignIn(googleUser)
     //$("#pic").attr('src',profile.getImageUrl());
     //$("#email").text(profile.getEmail());
     //window.location.replace("templates/userPage.html");
-          }
+
+}
+
+
