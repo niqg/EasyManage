52b5c2dd407ab6988ecac6f492cebf8f0892181e
00Bren00 <ringelb8@students.rowan.edu>
2018-04-14 19:41:38 -0400

    Update gSignIn.js
    fixed issues stemming from change to application.py endpoints. 
    
    Also, pressing cancel now requires the user to press sign in again, instead of displaying the enter orgName popup again. 
    
    also, a popup telling the user why there is an error comes up if the user enters the empty string or the "defaultOrgName" string.

--- FlaskApp/static/js/gSignIn.js
+++ FlaskApp/static/js/gSignIn.js
@@ -7,17 +7,47 @@ function onSignIn(googleUser)
     var profile = googleUser.getBasicProfile();
     var theUserEmail = profile.getEmail();
     console.log(theUserEmail);
-
+    
+    var googleToken = theUserEmail;//googleUser.getId();// should be changed to be verified by backend at some point. https://developers.google.com/identity/sign-in/web/backend-auth
+    
+    
+    //Hijacked, LOL
+    $.ajax({url:"/login", type: "POST", data: {google_client_id: googleToken, email:theUserEmail}, success: function(data) {//Login if i can
+        console.log(data.key);
+        if(data.key == SUCCESS_KEY)
+        {        
+            window.location = "/home";    //login successful
+        }
+        else                              //prompt user to create an account. 
+        {
+            var orgName = window.prompt("What do you want to name your organization?","defaultOrgName");
+            
+            while(orgName == "" || orgName == "defaultOrgName")                        //make sure they give us good information. 
+            {
+                alert("Please Enter a valid organization Name.");
+                orgName = window.prompt("What do you want to name your organization?","defaultOrgName");
+            }
+            
+            if(orgName != null)    //orgName is null if they pressed cancel // not null if they pressed okay. 
+            { 
+                //create account
+                $.ajax({url:"/account/new", type: "POST", data: {google_client_id: googleToken, userEmail: theUserEmail, userType:"ORG", org_name:orgName}, success: function(data) {}});
+                //navigate to home page. 
+                $.ajax({url:"/login", type: "POST", data: {google_client_id: googleToken, email:theUserEmail}, success: function(data) {
+                    window.location = "/home";
+                }});
+            }//else they pressed cancel - refresh the page.         
+        }
+    }});    
+}    
+    
+/*
 
     $.ajax({url:"/users/get", type: "GET", data: {userEmail: theUserEmail}, success: function(data) {
         //console.log(data.key);
         if(data.key == SUCCESS_KEY) //user exists
         {
-            var googleToken = "tempID";
-            //Hijacked, LOL
-            $.ajax({url:"/login", type: "POST", data: {google_client_id: googleToken, email:theUserEmail}, success: function(data) {//This line of code
-            window.location = "/home";
-            }});
+
         }
         else                        //prompt user to make an account
         {
@@ -39,7 +69,7 @@ function onSignIn(googleUser)
             //modal.innerhtml = "";
         
         }
-    }});
+    }});*/
     
 
     //var profile=googleUser.getBasicProfile();
@@ -48,7 +78,5 @@ function onSignIn(googleUser)
     //$("#pic").attr('src',profile.getImageUrl());
     //$("#email").text(profile.getEmail());
     //window.location.replace("templates/userPage.html");
-
-}
 
 
