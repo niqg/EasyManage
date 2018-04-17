122e75ea4a35ef63cc590ad52940e9786a8b34d0
natedesimone <desimonen2@students.rowan.edu>
2018-04-17 14:28:19 -0400

    Add files via upload

---
+++ FlaskApp/static/js/workOrders/getWorkOrders.js
@@ -0,0 +1,77 @@
+function getWorkOrders() {
+    setState("Work Orders");
+    toggleCalendar();
+    $(function()
+    {
+        $.getJSON("/entries/work", {},
+        function(data) {
+            console.log("The Get WORK ORDERS script has been fired");
+	
+            if(data.key === 1){
+		console.log("the Data key was " + data.key + "...aborting");
+		var table = document.getElementById("tableid");
+		table.innerHTML = data.message;
+		return;
+	    }
+
+	    //panel where the table is inserted
+            var panel = document.getElementById("panelBody");
+
+            // Find a <table> element with id="myTable":
+            var table = document.getElementById("tableid");
+            table.innerHTML = "";
+            // Create an empty <thead> element and add it to the table:
+            var header = table.createTHead();
+
+            // Create an empty <tr> element and add it to the first position of <thead>:
+            var row = header.insertRow(0);
+
+            // Insert a new cell (<td>) at the first position of the "new" <tr> element:
+            var cell = row.insertCell(0);
+            var cell1 = row.insertCell(1);
+            var cell2 = row.insertCell(2);
+            var cell3 = row.insertCell(3);
+            var cell4 = row.insertCell(4);
+            var cell5 = row.insertCell(5);
+
+            // Add some bold text in the new cell:
+            cell.innerHTML = "<b>Entry ID</b>";
+            cell1.innerHTML = "<b>Title</b>";
+            cell2.innerHTML = "<b>Date Created</b>";
+            cell3.innerHTML = "<b>Status</b>";
+            cell4.innerHTML = "<b>Date Completed</b>";
+            cell5.innerHTML = "<b>Description</b>";
+            cell5.width = "40%"
+	    var contentHeader = document.getElementById("entryHeader");
+    		contentHeader.innerHTML = "Work Orders";
+                          var json = data.data
+	tblbody = document.createElement("tbody");
+            for(var i = 0; i < json.length; i++) {
+                var obj = json[i];
+
+                var row = tblbody.insertRow();
+                var cell0 = row.insertCell(0);
+                var cell1 = row.insertCell(1);
+                var cell2 = row.insertCell(2);
+                var cell3 = row.insertCell(3);
+                var cell4 = row.insertCell(4);
+                var cell5 = row.insertCell(5);
+
+                cell0.innerHTML = obj[0];	//entry ID
+                cell1.innerHTML = obj[1];	//Title
+                cell2.innerHTML =  obj[2];	//date created
+                cell3.innerHTML =  obj[4]; //status (completed or not)
+                cell4.innerHTML =  obj[5]; //Type
+                cell5.innerHTML =  obj[3];	//description
+
+		table.appendChild(tblbody);
+            }
+
+
+
+        }
+        ).fail(function() {
+            console.log("Fail");
+        });
+    });
+}
